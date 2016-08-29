520a309371eae55f506e93bdccdfcf83e5362b2e
Mike R <mikeup28@gmail.com>
2016-08-29 10:27:32 -0700

    Gets and downloads list of all safari extensions
    Gets and downloads list of all safari extensions. Can use this data to download and analyze the data in each extension

---
+++ getSafariExt.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#Author: Michael Radov
+#Date 8/22/2016
+#Description: Download a list of all of the Safari Extensions
+# They can later be downloaded, extracted, and analyzed
+
+import urllib2, os, bs4
+
+#getting everything setup
+allExts=open('allExts.txt', 'a')
+
+
+#----------------------------------------------------------
+#Get list of all available extensions
+#----------------------------------------------------------
+url="https://safari-extensions.apple.com/extensions/extensions.json"
+
+get = urllib2.urlopen(url).read()
+dom = bs4.BeautifulSoup(get, "html.parser")
+
+downloadURL="https://safari-extensions.apple.com/extensions/"
+
+
+allExts.write("To download an extension, the url is:\n" + downloadURL + "com.whatever/file name\nReplace com.name with the com.name of the file.\nReplace the file name with the .safariextz file name\n")
+#write data to the file
+allExts.write(dom.get_text())
